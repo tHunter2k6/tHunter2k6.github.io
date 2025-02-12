c8dea71cd0730e938b8f2ebd76b3c837ad9ef00a
Sanan Sheikh <sanansheikh130306@gmail.com>
2025-02-12 10:57:47 +0500

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3733263681"
+    serviceWorkerVersion: "3814240161"
   }
 });
